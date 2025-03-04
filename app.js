3086a6e40fc5f04dac33ff0365a2c813605956c6
Shehar Mavitha <mavithashehar@gmail.com>
2025-03-05 01:13:38 +0530

    create app and user

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const connectDB = require('./config/db'); 
+const userhRoutes = require('./routers/userRoutes')
+//const postRoutes = require('./routers/postRoutes')
+app.use(express.json());
+
+connectDB();
+
+app.use('/api',userhRoutes);
+//app.use("/api/posts", postRoutes);
+
+const port = process.env.PORT || 3000;
+app.listen(port);
